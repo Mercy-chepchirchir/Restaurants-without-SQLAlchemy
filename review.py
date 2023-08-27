136c1885e87ecaf91f402c6133c29959aad0c014
mercy-chepchirchir <mercychepchirchir33@gmail.com>
2023-08-27 18:56:40 +0300

    created a decorator classmethod and a method all

--- review.py
+++ review.py
@@ -26,4 +26,27 @@ class Review:
     
     rating = property(get_rating, set_rating)
             
-   
+    # the decorator @classmethod defines the class method/indicates that the method is a class
+    #  the  method all() takes the class itself as the an argument(cls) method returns all reviews that have been created
+    @classmethod
+    def all(cls):
+        printedReviews = []
+        for review in cls.all_reviews:
+            printedReviews.append(f"Customer: {review.customer}, Rating: {review.rating}")
+
+        return printedReviews
+    
+# creating an instance of the review class
+myreview = Review("mercy", "Copper Ivy", 12)
+newreview = Review("Chirie", "Red Ginger", 10)
+
+# getting the ratings of the reviews using the rating() method
+print(myreview.rating)
+
+# getting all the reviews
+all_reviews = Review.all()
+print(all_reviews)
+
+        
+        
+        
