37f6f0e13ee95cfb322d51674e2579aabde95516
mercy-chepchirchir <mercychepchirchir33@gmail.com>
2023-08-27 23:38:55 +0300

    created methods add_review and review

--- restaurant.py
+++ restaurant.py
@@ -1,6 +1,10 @@
+from review import Review
+from customer import Customer
+
 class Restaurant:
     def __init__(self,name=None):
         self._name = None
+        self._reviews = []
         if name is not None:
             self.set_name(name)
         
@@ -9,11 +13,24 @@ class Restaurant:
             self._name = name
             
     def name (self):
-        return self._name 
-     
-restaurant = Restaurant("Copper Ivy")   
-print(restaurant.name())
-
+        return self._name
     
-        
+    def reviews(self):
+        return self._reviews
+    
+    def add_review(self, customer, rating):
+        self._reviews.append(Review(customer, self, rating))
+    
+    
+     
+restaurant1 = Restaurant("Copper Ivy")   
+# print(restaurant1.name())
+
+customer1 = Customer("Mercy", "Tonui")
+
+restaurant1.add_review(customer1, 8)
+restaurant1.add_review(customer1, 4)
+
+for review in restaurant1.reviews():
+    print(review.rating)
         
