9782eaec9688bc3606760592073c56d4532dff41
mercy-chepchirchir <mercychepchirchir33@gmail.com>
2023-08-28 00:23:07 +0300

    add unique list of customers when creating review

--- restaurant.py
+++ restaurant.py
@@ -5,6 +5,7 @@ class Restaurant:
     def __init__(self,name=None):
         self._name = None
         self._reviews = []
+        self._reviewed_customers = []
         if name is not None:
             self.set_name(name)
         
@@ -20,17 +21,28 @@ class Restaurant:
     
     def add_review(self, customer, rating):
         self._reviews.append(Review(customer, self, rating))
-    
-    
+        
+        # check if the customer passed in as argument when creating review exists in list of reviewed customers
+        if not any(rev_customer.full_name == customer.full_name for rev_customer in self._reviewed_customers):
+            self._reviewed_customers.append(customer)
+            
+                
+    def customers(self):
+        return self._reviewed_customers
+        
      
 restaurant1 = Restaurant("Copper Ivy")   
 # print(restaurant1.name())
 
 customer1 = Customer("Mercy", "Tonui")
+customer2 = Customer("Potato", "Chips")
 
 restaurant1.add_review(customer1, 8)
 restaurant1.add_review(customer1, 4)
+restaurant1.add_review(customer2, 10)
 
-for review in restaurant1.reviews():
-    print(review.rating)
-        
+# for review in restaurant1.reviews():
+#     print(review.rating)
+    
+for customer in restaurant1.customers():
+    print(customer)
