c9b5a4efcd79d42cd16368a630753a5b38234677
mercy-chepchirchir <mercychepchirchir33@gmail.com>
2023-08-27 18:53:01 +0300

    created a class review and initialised it

--- review.py
+++ review.py
@@ -1,13 +1,13 @@
 class Review:
-    def __init__ (self, customer, restaurant, rating):
+# all reviews is an empty list so that it will be used to store all instances of reviews.
+    all_reviews = [] 
+    
+    def __init__ (self, customer, restaurant, rating=None):
         self.customer = customer
         self.restaurant = restaurant
-        self.rating = rating
-        
-    def rating(self):
-        return self.rating
-    
-    
-        
-        
-        
+        self._rating = None
+        Review.all_reviews.append(self)  # Adding the review to the all_reviews list
+
+        if rating is not None:
+            self.set_rating(rating)
+  
