28c9afe088204fde5e3afa3a0197423bb04fad2a
mercy-chepchirchir <mercychepchirchir33@gmail.com>
2023-08-28 22:05:03 +0300

    created the customer and restaurant function

--- review.py
+++ review.py
@@ -3,8 +3,8 @@ class Review:
     all_reviews = [] 
     
     def __init__ (self, customer, restaurant, rating=None):
-        self.customer = customer
-        self.restaurant = restaurant
+        self._customer = customer
+        self._restaurant = restaurant
         self._rating = None
         Review.all_reviews.append(self)  # Adding the review to the all_reviews list
 
@@ -22,19 +22,20 @@ class Review:
      # return the rating
     def get_rating(self):
         return self._rating   
-     
-    
+
     rating = property(get_rating, set_rating)
+    
+    def customer(self):
+        return self._customer
+    
+    def restaurant(self):
+        return self._restaurant
             
     # the decorator @classmethod defines the class method/indicates that the method is a class
     #  the  method all() takes the class itself as the an argument(cls) method returns all reviews that have been created
     @classmethod
     def all(cls):
-        printedReviews = []
-        for review in cls.all_reviews:
-            printedReviews.append(f"Customer: {review.customer}, Rating: {review.rating}")
-
-        return printedReviews
+        return cls.all_reviews
     
 # creating an instance of the review class
 myreview = Review("mercy", "Copper Ivy", 12)
@@ -42,6 +43,7 @@ newreview = Review("Chirie", "Red Ginger", 10)
 
 # getting the ratings of the reviews using the rating() method
 print(myreview.rating)
+print(newreview.rating)
 
 # getting all the reviews
 all_reviews = Review.all()
