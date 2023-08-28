a2a4b9bb1d35b0afce57087a3b76ace3a7030990
mercy-chepchirchir <mercychepchirchir33@gmail.com>
2023-08-28 23:15:29 +0300

    created a find by name function returns the first customer

--- customer.py
+++ customer.py
@@ -1,3 +1,6 @@
+from review import Review
+from restaurant import Restaurant
+
 class Customer:
     all_customers = [] #used to store all instances of customer
     
@@ -5,6 +8,8 @@ class Customer:
         self._given_name= given_name
         self._family_name = family_name
         self._full_name = None
+        self._reviews = []
+        self._restaurants = []
         Customer.set_full_name(self)
         Customer.all_customers.append(self)
         
@@ -34,6 +39,36 @@ class Customer:
         return self._full_name
     
     full_name = property(get_full_name, set_full_name)
+    
+    def restaurants(self):
+        reviewed_restaurants = []
+        for review in Review.all():
+            if review.customer() == self:
+                reviewed_restaurants.append(review.restaurant())
+        return set(reviewed_restaurants)
+                
+    def add_review(self, restaurant, rating):
+        new_review = Review(self._full_name, restaurant.name(), rating)
+        self._reviews.append(new_review)            
+        restaurant._reviews.append(new_review)
+        
+    def num_reviews(self):
+        return len(self._reviews)
+
+    @classmethod
+    def find_by_name(cls, name):
+        for customer in cls.all_customers:
+            if customer.full_name == name:
+                return customer
+        return None
+
+    @classmethod
+    def find_all_by_given_name(cls, name):
+        matching_customers = []
+        for customer in cls.all_customers:
+            if customer.full_name.split()[0] == name:
+                matching_customers.append(customer)
+        return matching_customers
             
 # the all class method returns the list of all customer instances          
     @classmethod
@@ -56,3 +91,12 @@ for customer in all_customers:
 
 print(all_customers)
 print(mycustomer)
+
+
+restaurant1 = Restaurant("Cooper VY")
+
+customer1 = Customer("Chiri", "Tonui")
+customer1.add_review(restaurant1, 7)
+
+for review in restaurant1.reviews():
+    print(review.rating)
