8977fc0930ff4734a623152aa9524d76e5fb852d
mercy-chepchirchir <mercychepchirchir33@gmail.com>
2023-08-28 23:22:40 +0300

    create method that calculates average star rating

--- restaurant.py
+++ restaurant.py
@@ -1,5 +1,4 @@
 from review import Review
-from customer import Customer
 
 class Restaurant:
     def __init__(self,name=None):
@@ -17,32 +16,33 @@ class Restaurant:
         return self._name
     
     def reviews(self):
-        return self._reviews
-    
-    def add_review(self, customer, rating):
-        self._reviews.append(Review(customer, self, rating))
-        
-        # check if the customer passed in as argument when creating review exists in list of reviewed customers
-        if not any(rev_customer.full_name == customer.full_name for rev_customer in self._reviewed_customers):
-            self._reviewed_customers.append(customer)
-            
+        myreviews = []
+        for review in Review.all():
+            if review.restaurant() == self:
+               myreviews.append(review)
+        return set(myreviews)     
                 
     def customers(self):
-        return self._reviewed_customers
+        reviewed_customers = []
+        for review in Review.all():
+            if review.restaurant() == self:
+              reviewed_customers.append(review.customer())
+        return set(reviewed_customers)
+    
+    def average_star_rating(self):
+        if not self._reviews:
+            return 0.0
+        total_rating = sum(review.rating for review in self._reviews)
+        return total_rating / len(self._reviews)
+
+        
         
      
 restaurant1 = Restaurant("Copper Ivy")   
 # print(restaurant1.name())
-
-customer1 = Customer("Mercy", "Tonui")
-customer2 = Customer("Potato", "Chips")
-
-restaurant1.add_review(customer1, 8)
-restaurant1.add_review(customer1, 4)
-restaurant1.add_review(customer2, 10)
-
-# for review in restaurant1.reviews():
-#     print(review.rating)
     
-for customer in restaurant1.customers():
-    print(customer)
+# for customer in restaurant1.customers():
+#     print(customer)
+
+avg_rating = restaurant1.average_star_rating()
+print(f"Average star rating for {restaurant1.name()}: {avg_rating}")
